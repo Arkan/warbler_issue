307946578fe45cf7ae2c4e590e52692a08ac8c4f
Arkan <florian.bertholin@gmail.com>
2013-12-06 15:13:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+## Compilation && Execution
+
+    bundle exec warble executable jar && java -jar test-warbler.jar
---
+++ bin/application.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+
+loop {
+  puts "COUCOU"
+  sleep 1
+}
